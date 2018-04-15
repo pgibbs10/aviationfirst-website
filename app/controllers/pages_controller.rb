dec3b91bfb35d64be68607fe49ccc77496ec9430
Paul Gibbs <pwgibbs10@gmail.com>
2018-04-15 13:58:30 -0700

    Created contact page, updated production key again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
 	def homescreen
 	end
+
+	def contact
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+	get 'contact', to: 'pages#contact'
 	root to: 'pages#homescreen'
 end
