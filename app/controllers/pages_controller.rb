3c6dbc0bdd3e542faa13f8014725939b8851a186
Paul Gibbs <pwgibbs10@gmail.com>
2018-04-17 00:25:08 -0700

    Added more logo customized. Added more pages, added custom font

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
 	def homescreen
 	end
 
+	def about
+	end	
+
+	def charter
+	end	
+
 	def contact
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 	get 'contact', to: 'pages#contact'
+	get 'about', to: 'pages#about'
+	get 'charter', to: 'pages#charter'
 	root to: 'pages#homescreen'
 end
